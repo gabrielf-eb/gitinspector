5a2b031b5125283e93968a65c397e5a63c47c098
Gabriel Flores <gabrielf@eventbrite.com>
2020-07-07 16:46:14 -0300

    main

--- cloneall.sh
+++ cloneall.sh
@@ -1,4 +1,4 @@
 GHORG=eventbrite;
-for page in $(seq 1 20); do curl "https://api.github.com/orgs/$GHORG/repos?access_token=339f6c8dd006632ccd35060d73ffe10ba93341d7&per_page=2000&page="+$page | grep -o 'git@[^"]*' | xargs -L1 git clone; done
-# for page in $(seq 1 20); do curl "https://api.github.com/orgs/$GHORG/repos?access_token=339f6c8dd006632ccd35060d73ffe10ba93341d7&per_page=2000&page="+$page | grep -o 'git@[^"]*service' | xargs -L1 git clone; done
+for page in $(seq 1 30); do curl "https://api.github.com/orgs/$GHORG/repos?access_token=&per_page=2000&page="+$page | grep -o 'git@[^"]*' | xargs -L1 git clone; done
+# for page in $(seq 1 20); do curl "https://api.github.com/orgs/$GHORG/repos?access_token=&per_page=2000&page="+$page | grep -o 'git@[^"]*service' | xargs -L1 git clone; done
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from datetime import datetime, timedelta
 
 from pyhive import presto
@@ -18,7 +19,7 @@ def get_presto():
 
 
 def presto_get_repos():
-    # cursor.execute('Select * from team_comms_eng.eb_commits_author')
+    # c
     # cursor.execute('INSERT INTO hive.team_comms_eng.eb_commits_repos values (61, \'legacy_search_service\', \'service\', \'TRUE\', \'SOA\')')
     # print(cursor.fetchall())
     # cursor.execute('Select * from team_comms_eng.eb_commits_repos')
@@ -27,7 +28,7 @@ def presto_get_repos():
     return cursor.fetchall()
 
 
-def presto_insert(sql):
+def presto_execute(sql):
     cursor = get_presto()
     cursor.execute(sql)
     print(cursor.fetchall())
@@ -87,12 +88,142 @@ def process():
                     for insert_page in range(0, len(inserts), page_size):
                         print('Inserting {} {} {}'.format(repo, insert_page, insert_page + page_size))
                         sql = ', '.join(inserts[insert_page:insert_page + page_size])
-                        presto_insert('INSERT INTO hive.team_comms_eng.eb_commits VALUES {}'.format(sql))
+                        presto_execute('INSERT INTO hive.team_comms_eng.eb_commits VALUES {}'.format(sql))
                 except Exception as e:
                     print(e)
             from_day = 1
             from_month = 1
 
 
+def update_unknown_repos():
+    cursor = get_presto()
+    cursor.execute(
+        'SELECT commit.repo '
+        'FROM team_comms_eng.eb_commits commit WHERE commit.repo not in (SELECT repo FROM team_comms_eng.eb_commits_repos repo) '
+        'GROUP BY commit.repo'
+    )
+    unknown_repos = cursor.fetchall()
+    cursor.execute('SELECT MAX(id) from team_comms_eng.eb_commits_repos')
+    last_repo_id = cursor.fetchall()[0][0]
+    inserts = []
+    for unknown_repo in unknown_repos:
+        last_repo_id += 1
+        sql = (
+            "("
+            "{}, "
+            "'{}', "
+            "'{}', "
+            "'{}', "
+            "'{}' "
+            ") "
+        )
+        inserts.append(
+            sql.format(
+                last_repo_id,
+                unknown_repo[0],
+                'service' if 'service' in unknown_repo[0] else 'library',
+                'TRUE' if 'service' in unknown_repo[0] else 'FALSE',
+                'PYSOA' if 'service' in unknown_repo[0] else 'LIB',
+            )
+        )
+    page_size = 500
+    for insert_page in range(0, len(inserts), page_size):
+        print('Inserting unknown_repos {} {}'.format(insert_page, insert_page + page_size))
+        sql = ', '.join(inserts[insert_page:insert_page + page_size])
+        presto_execute('INSERT INTO hive.team_comms_eng.eb_commits_repos VALUES {}'.format(sql))
+
+
+def update_unknown_authors():
+    cursor = get_presto()
+    cursor.execute(
+        'SELECT commit.email, commit.author, MAX(commit.date) '
+        'FROM team_comms_eng.eb_commits commit WHERE commit.email not in (SELECT email FROM team_comms_eng.eb_commits_author author) '
+        'GROUP BY commit.email, commit.author'
+    )
+    unknown_authors = cursor.fetchall()
+    cursor.execute('SELECT MAX(id) from team_comms_eng.eb_commits_author')
+    last_author_id = cursor.fetchall()[0][0]
+    inserts = []
+    for unknown_author in unknown_authors:
+        last_author_id += 1
+        sql = (
+            "("
+            "{}, "
+            "'{}', "
+            "'{}', "
+            "'', "
+            "'', "
+            "'' "
+            ") "
+        )
+        inserts.append(
+            sql.format(
+                last_author_id,
+                unknown_author[1],
+                unknown_author[0],
+            )
+        )
+    page_size = 500
+    for insert_page in range(0, len(inserts), page_size):
+        print('Inserting unknown_authors {} {}'.format(insert_page, insert_page + page_size))
+        sql = ', '.join(inserts[insert_page:insert_page + page_size])
+        presto_execute('INSERT INTO hive.team_comms_eng.eb_commits_author VALUES {}'.format(sql))
+
+
+def update_all_engs():
+    try:
+        presto_execute('DROP TABLE hive.team_comms_eng.eb_commits_author_2')
+    except Exception as e:
+        pass
+    presto_execute(
+        'CREATE TABLE hive.team_comms_eng.eb_commits_author_2 ('
+        ' id INTEGER, '
+        ' briteling VARCHAR(255), '
+        ' email VARCHAR(255), '
+        ' location VARCHAR(255), '
+        ' team VARCHAR(255), '
+        ' bu VARCHAR(255) '
+        ')'
+    )
+    with open('all_engs_updated2.csv') as f:
+        reader = csv.reader(f)
+        inserts = []
+        last_author_id = 0
+        for row in reader:
+            print(row)
+            last_author_id += 1
+            sql = (
+                "("
+                "{}, "
+                "'{}', "
+                "'{}', "
+                "'{}', "
+                "'{}', "
+                "'{}' "
+                ") "
+            )
+            inserts.append(
+                sql.format(
+                    last_author_id,
+                    row[1],
+                    row[2],
+                    row[5],
+                    row[6],
+                    row[7],
+                )
+            )
+    page_size = 500
+    for insert_page in range(0, len(inserts), page_size):
+        print('Inserting authors {} {}'.format(insert_page, insert_page + page_size))
+        sql = ', '.join(inserts[insert_page:insert_page + page_size])
+        presto_execute('INSERT INTO hive.team_comms_eng.eb_commits_author_2 VALUES {}'.format(sql))
+
+    presto_execute('ALTER TABLE hive.team_comms_eng.eb_commits_author RENAME TO hive.team_comms_eng.eb_commits_author_bak')
+    presto_execute('ALTER TABLE hive.team_comms_eng.eb_commits_author_2 RENAME TO hive.team_comms_eng.eb_commits_author')
+
+
 if __name__ == '__main__':
     process()
+    update_unknown_authors()
+    update_unknown_repos()
+    # update_all_engs()
